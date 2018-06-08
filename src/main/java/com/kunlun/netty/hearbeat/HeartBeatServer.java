ca97af6d6f0b7d1cbc268c0d04ccadce39cef8d8
Jarven <769558579@qq.com>
2018-06-08 23:28:24 +0800

    心跳检测

---
+++ src/main/java/com/kunlun/netty/hearbeat/HeartBeatServer.java
@@ -0,0 +1,31 @@
+package com.kunlun.netty.hearbeat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
+
+/**
+ * TODO:
+ *
+ * @author wangbinbin
+ * @version 1.0.0
+ * @date 2018/6/8 下午11:25
+ */
+public class HeartBeatServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(boosGroup, workGroup).channel(ServerSocketChannel.class).handler(null);
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            boosGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
